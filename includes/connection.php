3e60716a515c9b690e599c79d50e0cbaead16418
Rayanemksm1 <rayanemksm5@gmail.com>
2025-11-21 14:31:30 +0100

    Update database connection details in connection.php

--- includes/connection.php
+++ includes/connection.php
@@ -1,4 +1,12 @@
 <?php
-    $con = mysqli_connect("localhost","root","","constructionstore");
-    
+$host = "sql310.infinityfree.com";
+$user = "if0_40474606";
+$pass = "ohkUeUgjv97";  
+$db   = "if0_40474606_constructionstore";
+
+$conn = mysqli_connect($host, $user, $pass, $db);
+
+if (!$conn) {
+    die("Erreur de connexion MySQL : " . mysqli_connect_error());
+}
 ?>
